8dc85428457145cf650da565425c7aef9ed713cb
Roni Hytonen <fnt5778@varian.com>
2021-08-14 11:59:32 +0300

    Do 3.15 - 3.18

--- index.js
+++ index.js
@@ -15,30 +15,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms - 
 
 const Person = require('./models/person');
 
-let persons =
-  [
-    {
-      "id": 1,
-      "name": "Arto Hellas",
-      "number": "040-123456"
-    },
-    {
-      "id": 2,
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523"
-    },
-    {
-      "id": 3,
-      "name": "Dan Abramov",
-      "number": "12-43-234345"
-    },
-    {
-      "id": 4,
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122"
-    }
-  ];
-
 // #region GETTERS
 app.get('/',
   (request, response) => {
@@ -46,33 +22,40 @@ app.get('/',
   });
 
 app.get('/api/persons', (request, response) => {
-  Person.find({}).then(result => {
-    response.json(result);
-  })
+  Person.find({})
+    .then(result => {
+      response.json(result);
+    })
+    .catch(error => next(error))
 });
 
 app.get('/info', (request, response) => {
-  let res = 'Phonebook has info for ' + persons.length + ' people';
-  res += '<p> ' + Date(Date.now()).toString() + '<p>';
-  response.send(res);
-})
+  Person.find({})
+    .then(result => {
+      let res = 'Phonebook has info for ' + result.length + ' people';
+      res += '<p> ' + Date(Date.now()).toString() + '<p>';
+      response.send(res);
+    })
+    .catch(error => next(error))
+});
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person);
-  });
-})
+  Person.findById(request.params.id)
+    .then(person => {
+      response.json(person);
+    })
+    .catch(error => next(error))
+});
 
 // #endregion GETTERS
 
-app.delete('/api/persons/:id', (request, response) => {
-  console.log('delete: ', request.params.id);
-
-  const id = Number(request.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  response.status(204).end();
-})
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+});
 
 app.post('/api/persons', (request, response) => {
   const body = request.body;
@@ -87,35 +70,56 @@ app.post('/api/persons', (request, response) => {
       error: 'Content number missing'
     });
   }
-  //if (persons.map(person => person.name).includes(person.name)) {
-  //  return response.status(409).json({
-  //    error: 'Conflict: Name must be unique'
-  //  });
-  //}
 
   const person = new Person({
     name: body.name,
     number: body.number
   });
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson);
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson);
+    })
+    .catch(error => next(error))
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+  console.log('Put request:', body);
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
   });
 
-  //person.id = Math.floor(Math.random() * 1e6);
-  //console.log('New person: ', person);
+  Person.findByIdAndUpdate(request.params.id, {number: body.number}, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
-  //persons = persons.concat(person);
-  //console.log('Persons: ', persons);
-
-  //response.json(person);
-});
+// #region Middleware
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+}
+
+app.use(errorHandler);
+
+// #endregion Middleware
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
